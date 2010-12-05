fdb804b0a98e8c094fe5982ea3db97f8da4645eb
Christopher Roach <vthakr@gmail.com>
2010-12-04 20:32:11 -0800

    Added hg and git as type options to the parse command

--- logparser/__init__.py
+++ logparser/__init__.py
@@ -1,8 +1,16 @@
 from svnparser import SvnParser
+from gitparser import GitParser
+from hgparser import HgParser
 
 def parse(source, type):
 	if type == "svn":
 		return SvnParser(source)
+	elif type == "git":
+		return GitParser(source)
+	elif type == "hg":
+		return HgParser(source)
+	else:
+		raise ValueError("Type '{0}' does not exist".format(type))
 		
 __all__ = ["parse"]
 		
