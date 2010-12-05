c2186dc2da384887a8aeb5857e98a32f1a3c5bd6
Christopher Roach <vthakr@gmail.com>
2010-12-04 17:23:36 -0800

    Added a parser for Mercurial logs

---
+++ logparser/hgparser.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from logparser import *
+from mercurial import ui, hg
+import os
+
+class HgParser(object):
+	def __init__(self, source):
+		super(HgParser, self).__init__()
+		if not os.path.exists(source) or os.path.isfile(source):
+			raise IOError("No such repository: '{0}'".format(source))
+		repo = hg.repository(ui.ui(), source)
+		self.logs = self._log_generator(repo)
+	
+	def __iter__(self):
+		return self
+	
+	def next(self):
+		return next(self.logs)
+		
+	def _log_generator(self, repo):
+		for i in repo:
+			log = repo[i]
+			yield Log(
+				revision = log.rev(),
+				author   = log.user(),
+				comment  = log.description(),
+				files    = log.files()
+			)
