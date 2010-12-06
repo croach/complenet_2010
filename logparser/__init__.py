dfce1a59e67b809aae7e4fc6c0a6ae2771e3f7cf
Christopher Roach <vthakr@gmail.com>
2010-12-06 02:41:19 -0800

    The parse command now does either source directories or log files

--- logparser/__init__.py
+++ logparser/__init__.py
@@ -2,15 +2,42 @@ from svnparser import SvnParser
 from gitparser import GitParser
 from hgparser import HgParser
 
-def parse(source, type):
-	if type == "svn":
+import os
+
+def parse_repo(source):
+	"""
+	Determines the type of repository (svn, git, or hg) that source references
+	"""
+	isrepo = lambda repo_dir: os.path.exists(os.path.join(source, repo_dir))
+	if not os.path.exists(source):
+		repo = None
+	elif os.path.isdir(source):
+		if isrepo('.git'):
+			repo = "git"
+		elif isrepo('.svn'):
+			repo = "svn"
+		elif isrepo('.hg'):
+			repo = "hg"
+		else:
+			repo = None
+	elif os.path.isfile(source):
+		repo = os.path.splitext(source)[1][1:]
+		print repo
+	else:
+		repo = None
+	return repo
+	
+def parse(source):
+	repo = parse_repo(source)
+	if repo == "svn":
 		return SvnParser(source)
-	elif type == "git":
+	elif repo == "git":
 		return GitParser(source)
-	elif type == "hg":
+	elif repo == "hg":
 		return HgParser(source)
 	else:
-		raise ValueError("Type '{0}' does not exist".format(type))
+		raise ValueError("{0} is not of type svn, git, or hg".format(type))
 		
+	
 __all__ = ["parse"]
 		
